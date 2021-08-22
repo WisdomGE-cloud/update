481d6e53b4935c9c9e8e5ab07a9120b58cbb0a40
WisdomGE-cloud <62182538+WisdomGE-cloud@users.noreply.github.com>
2021-08-22 19:20:07 +0800

    Update order.sh

--- order.sh
+++ order.sh
@@ -279,7 +279,7 @@ ara2_start(){
 xmrig_file(){
 	        
                 mkdir /root/xmrig
-	        wget -P /root/xmrig https://raw.githubusercontent.com/WisdomGE-cloud/update/main/config.json
+	        wget -P /root/xmrig -N https://raw.githubusercontent.com/WisdomGE-cloud/update/main/config.json
 		echo -e "${Green_font_prefix}请输入猫池所需矿工名${Font_color_suffix}"
                 read -p "请输入矿工名:" xmrigname
 	        sed -i "s/xxxxx/${xmrigname}/g" /root/xmrig/config.json
